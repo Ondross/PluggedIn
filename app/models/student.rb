0b338e6e8b00cbefe89a054ed30c247fbea48cad
Andrew Heine <sfufetus@gmail.com>
2012-01-27 17:45:00 -0500

    student requirements

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
-validates_presence_of :name, :last_name, :address, :email
+validates_presence_of :name, :last_name, :address, :email, :town, :zip, :home_phone, :date_of_birth, :parent_email
 has_many :participants
 end
 
